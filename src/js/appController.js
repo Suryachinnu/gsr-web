ab147d4d6945f0b95d90dc8244427b28ee937e47
Sharief123 <janisherry9666@gmail.com>
2021-08-25 21:02:03 +0530

    padding and font-size for home & about us Pages

--- src/js/appController.js
+++ src/js/appController.js
@@ -37,14 +37,14 @@ define(['knockout', 'ojs/ojcontext', 'ojs/ojmodule-element-utils', 'ojs/ojknocko
         { path: '', redirect: 'home' },
         { path: 'home', detail: { label: 'Home', iconClass: 'oj-ux-ico-bar-chart' } },
         { path: 'about', detail: { label: 'About Us', iconClass: 'oj-ux-ico-fire' } },
-        { path: 'focus', detail: { label: 'Our Focus Area', iconClass: 'oj-ux-ico-contact-group' } },
+        { path: 'focus', detail: { label: 'Our Focus Areas', iconClass: 'oj-ux-ico-contact-group' } },
         { path: 'contact', detail: { label: 'Contact Us', iconClass: 'oj-ux-ico-information-s' } },
         { path: 'joinus', detail: { label: 'Join Us', iconClass: 'oj-ux-ico-information-s' } }
       ];
       let navData1 = [
         { path: 'home', detail: { label: 'Home', iconClass: 'oj-ux-ico-bar-chart' } },
         { path: 'about', detail: { label: 'About Us', iconClass: 'oj-ux-ico-fire' } },
-        { path: 'focus', detail: { label: 'Our Focus Area', iconClass: 'oj-ux-ico-contact-group' } },
+        { path: 'focus', detail: { label: 'Our Focus Areas', iconClass: 'oj-ux-ico-contact-group' } },
         { path: 'contact', detail: { label: 'Contact Us', iconClass: 'oj-ux-ico-information-s' } },
       ];
 
